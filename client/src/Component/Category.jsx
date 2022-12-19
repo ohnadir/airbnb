dd105033a5ea801f2dcf15318c9503aa6ac90f71
Nadir Ahmed <qx.nadir@gmail.com>
2022-12-19 10:43:10 +0600

    slider css implement

--- client/src/Component/Category.jsx
+++ client/src/Component/Category.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Slider from "react-slick";
+import '../Style/Category.css'
 import { CgOptions } from 'react-icons/cg';
 import {TbPool, TbBeach, TbToolsKitchen } from 'react-icons/tb'
 import { GiIsland, GiCampingTent, GiGrandPiano, 
@@ -9,6 +10,7 @@ import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 
 const Category = () => {
     const [keyword, setKeyword] = useState();
+    const [open, setOpen] = useState(false)
     const category = [
         {id: 1, name: "Pool", icon: <TbPool /> },
         { id: 2,  name: "Beach", icon: <TbBeach /> },
@@ -79,7 +81,7 @@ const Category = () => {
         ]
     }
   return (
-    <div>
+    <div style={{"top" : "86px", "zIndex" : "2"}} className='container mx-auto px-4 mt-5  overflow-y-hidden  sticky  bg-white'>
         <div className='flex items-center gap-10 justify-between pt-5'>
             <div className='relative  px-8 w-[80%] overflow-y-hidden'>
                 <Slider {...settings}>
@@ -97,11 +99,12 @@ const Category = () => {
                 </Slider>
             </div>
             <div className='w-[10%] hidden sm:block'>
-                <div  className='cursor-pointer flex items-center gap-1 font-semibold border rounded-lg max-w-fit px-5 py-2'>
+                <div onClick={()=>setOpen(!open)} className='cursor-pointer flex items-center gap-1 font-semibold border rounded-lg max-w-fit px-5 py-2'>
                     <CgOptions/> <span>Filter</span>
                 </div>
             </div>
         </div>
+        
     </div>
   )
 }
